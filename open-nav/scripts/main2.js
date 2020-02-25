fe9dfd5cbee4bf7c90db04d4e2691858af4fa881
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2020-02-25 11:49:55 +0100

    loops

--- open-nav/scripts/main2.js
+++ open-nav/scripts/main2.js
@@ -1,20 +1,12 @@
-const openMenuButton = document.querySelector('#icon-open');
-const closeMenuButton = document.querySelector('#icon-close');
 const header = document.querySelector('header');
-const links = document.querySelectorAll('nav a');
-const main = document.querySelector('main');
 
-function closeNav() {
-    if (header.classList.contains('nav-open')) {
-        header.classList.remove('nav-open');
-    }
-}
+document.querySelector('#icon-open').onclick = () => header.classList.add('nav-open');
 
-openMenuButton.addEventListener('click', () => {
-    header.classList.add('nav-open');
-});
-
-const closeElements = [main, closeMenuButton, ...links];
-closeElements.forEach(link => {
-    link.addEventListener('click', closeNav);
-});
+[
+    document.querySelector('main'),
+    document.querySelector('#icon-close'),
+    ...document.querySelectorAll('nav a'),
+].forEach(link =>
+    link.onclick = () =>
+        header.classList.remove('nav-open')
+);
