78810f953f281841e1b27e9ef22e5e0a3967e11f
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2020-02-28 13:58:52 +0100

    todo

--- open-nav/scripts/main.js
+++ open-nav/scripts/main.js
@@ -1,26 +1,16 @@
 // manage open and close nav
-const openMenuButton = document.querySelector('#icon-open');
-const closeMenuButton = document.querySelector('#icon-close');
 const header = document.querySelector('header');
-const links = document.querySelectorAll('nav a');
-const main = document.querySelector('main');
 
-function closeNav() {
-    if (header.classList.contains('nav-open')) {
-        header.classList.remove('nav-open');
-    }
-}
+document.querySelector('#icon-open')
+    .addEventListener('click', () => header.classList.add('nav-open'));
 
-openMenuButton.addEventListener('click', function () {
-    header.classList.add('nav-open');
-});
-
-closeMenuButton.addEventListener('click', closeNav);
-
-for (let i = 0; i < links.length; i++) {
-    links[i].addEventListener('click', function () {
-        closeNav();
-    });
-}
-
-main.addEventListener('click', closeNav);
+[
+    document.querySelector('main'),
+    document.querySelector('#icon-close'),
+    ...document.querySelectorAll('nav a')
+]
+    .forEach(item =>
+        item.addEventListener('click', () =>
+            header.classList.contains('nav-open') ?
+                header.classList.remove('nav-open') : ''
+        ));
