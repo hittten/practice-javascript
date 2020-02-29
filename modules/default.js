1eb00a8858f48508346e46916298912bb603ee5d
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2020-02-29 16:50:33 +0100

    modules

---
+++ modules/default.js
@@ -0,0 +1,63 @@
+export const data = [
+  {
+    name: "Lindy",
+    breed: "Cymric",
+    color: "white",
+    kittens: [
+      {
+        name: "Percy",
+        gender: "m"
+      },
+      {
+        name: "Thea",
+        gender: "f"
+      },
+      {
+        name: "Annis",
+        gender: "f"
+      }
+    ]
+  },
+  {
+    name: "Mina",
+    breed: "Aphrodite Giant",
+    color: "ginger",
+    kittens: [
+      {
+        name: "Doris",
+        gender: "f"
+      },
+      {
+        name: "Pickle",
+        gender: "f"
+      },
+      {
+        name: "Max",
+        gender: "m"
+      }
+    ]
+  },
+  {
+    name: "Antonia",
+    breed: "Ocicat",
+    color: "leopard spotted",
+    kittens: [
+      {
+        name: "Bridget",
+        gender: "f"
+      },
+      {
+        name: "Randolph",
+        gender: "m"
+      }
+    ]
+  }
+];
+
+export default function myDefaultSayHello() {
+  console.log('say default to you!');
+}
+
+export function noDefaultSayHello() {
+  console.log('say no default to you!');
+}
